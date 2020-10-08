dd76eb26a1ea821f95ed764ceea1a47b6d08694f
Jonathan Ferreira <jhouus33@gmail.com>
2020-10-08 09:18:17 -0300

    aqs ex 2 e 4 atualizados

--- src/ex_07_10/ex4.java
+++ src/ex_07_10/ex4.java
@@ -1,27 +1,30 @@
 package ex_07_10;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
+
 
 public class ex4 {
 	public static void main (String args[]) {
 		Scanner read = new Scanner (System.in);
-		int numero, raizQuadrada, eveladoAoQuadrado;
-		
+		int numero, elevadoAoQuadrado;
+		double raizQuadrada;
+		DecimalFormat df = new DecimalFormat("#.000");
 		
 		System.out.println("Digite qualquer número: ");
 		numero = read.nextInt();
 			
 				
 		if(numero % 2 == 0) {
-		raizQuadrada = (int) Math.sqrt(numero);
+		raizQuadrada = Math.sqrt(numero);
 		System.out.println("Número PAR");
-		System.out.println("Sua raiz² é " + raizQuadrada);
+		System.out.println("Sua raiz² é " + df.format(raizQuadrada));
 		
 		}
 		else {
-			eveladoAoQuadrado = numero*numero;
+			elevadoAoQuadrado = numero*numero;
 			System.out.println("Número ÍMPAR");
-			System.out.println("Elevado ao quadrado resulta " + eveladoAoQuadrado);
+			System.out.println("Elevado ao quadrado resulta " + elevadoAoQuadrado);
 		}	
 		
 				
